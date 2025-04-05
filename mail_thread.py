7f8e846fcaad139417fe7fa8b906522b4fb2a341
Dhanya <dhanya@cybrosys.info>
2025-04-05 15:32:45 +0530

    [updt]

--- mail_thread.py
+++ mail_thread.py
@@ -4,5 +4,5 @@ from odoo import models, fields
 class MailThread(models.AbstractModel):
     _inherit = 'mail.thread'
 
-    prod_list_ids = fields.Many2many('product.template', 'mail_thread_product_tmpl_rel')
+
 
