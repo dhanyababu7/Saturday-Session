65ab4bff04560914a1960779aa59c52dbd4a74d8
Dhanya <dhanya@cybrosys.info>
2025-04-05 14:57:23 +0530

    [ADD]

---
+++ coc.py
@@ -0,0 +1,17 @@
+def is_valid(s):
+  print('Started')
+  print(type(str(s)),'------')
+  list=[]
+  for i in range(s):
+    print(i,'iiiiii')
+    list.append(i)
+    if i == '(' :
+	      print('pp[p[p[')
+    elif i ==  '{' :
+	     print(']]]]]]]]]]]]')
+    elif i ==  '[':
+	     print('=====')
+    
+print(list,'list list')
+is_valid(212444)
+
